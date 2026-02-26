21928b07ffda9fc3973685fac5ab646684f38b02
the-koroche <thekoroche@gmail.com>
2026-02-26 09:53:01 +0200

    Update Event: rewrite Javadoc and add toString() implementation

--- src/main/java/org/theko/events/Event.java
+++ src/main/java/org/theko/events/Event.java
@@ -25,35 +25,84 @@
 package org.theko.events;
 
 /**
- * Base class for all events in the event-driven system.
+ * Abstract base class for all events.
  * <p>
- * Events are immutable data carriers that may be consumed to stop further handling.
+ * Events are immutable data carriers with a creation timestamp.
+ * They support consumption via `consume()` to prevent further propagation.
+ * 
+ * @see EventDispatcher
+ * @see EventConsumer
+ * @see Listener
  * 
  * @author Theko
  * @since 1.0
  */
 public abstract class Event {
 
-    /** Creation timestamp in milliseconds since UNIX epoch. */
-    protected final long timestamp = System.currentTimeMillis();
+    private final long timestamp;
+    private volatile boolean isConsumed;
 
-    /** Flag indicating whether this event has been consumed. */
-    private boolean isConsumed;
+    /**
+     *  Default constructor. 
+     */
+    protected Event() {
+        timestamp = System.currentTimeMillis();
+    }
 
-    protected Event() { }
-
-    /** Returns the creation timestamp of this event. */
+    /**
+     * Retrieves the creation timestamp of this event in milliseconds.
+     * <p>
+     * This is the time when this event was created, not when it was dispatched.
+     * 
+     * @return the creation timestamp of this event in milliseconds.
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
-    /** Marks this event as consumed to prevent further handling. */
-    public void consume() {
-        isConsumed = true;
+    /**
+     * Marks this event as consumed.
+     * <p>
+     * If this event has not been consumed yet, this method sets the consumed flag to true
+     * and returns true. Otherwise, it returns false.
+     * <p>
+     * Consuming an event prevents it from being further propagated to other listeners and consumers.
+     * 
+     * @return true if the event was successfully marked as consumed, false otherwise.
+     */
+    public boolean consume() {
+        if (!isConsumed) {
+            isConsumed = true;
+            return true;
+        }
+        return false;
     }
-
-    /** Checks whether this event has been consumed. */
+    
+    /**
+     * Indicates whether this event has been consumed or not.
+     * <p>
+     * A consumed event will not be propagated to other listeners.
+     * 
+     * @return true if this event has been consumed, false otherwise.
+     */
     public boolean isConsumed() {
         return isConsumed;
+    }
+
+    /**
+     * Returns a string representation of this event.
+     * <p>
+     * The returned string will be in the format "Event{timestamp=<i>timestamp</i>, isConsumed=<i>isConsumed</i>}"
+     * where <code>timestamp</code> is the creation timestamp of this event and
+     * <code>isConsumed</code> indicates whether this event has been consumed or not.
+     * 
+     * @return a string representation of this event.
+     */
+    @Override
+    public String toString() {
+        return "Event{" +
+                "timestamp=" + timestamp +
+                ", isConsumed=" + isConsumed +
+                '}';
     }
 }
